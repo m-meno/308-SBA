256079eece7591487b4046dddbc0738133557d4b
m-meno <mackenzie.meno@gmail.com>
2025-03-22 18:24:08 -0600

    added id objects

---
+++ notes.js
@@ -0,0 +1,100 @@
+//1.) If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
+// 2.) You should also account for potential errors in the data that your program receives. What if points_possible is 0? You cannot divide by zero. What if a value that you are expecting to be a number is instead a string? 
+// 3.) Use try/catch and other logic to handle these types of errors gracefully.
+// 4.) If an assignment is not yet due, do not include it in the results or the average. Additionally, if the learner’s submission is late (submitted_at is past due_at), deduct 10 percent of the total points possible from their score for that assignment.
+// 5.) Create a function named getLearnerData() that accepts these values as parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission]), and returns the formatted result, which should be an array of objects as described above.
+// 6.) You may use as many helper functions as you see fit.
+
+
+function scoreAve(a, b){
+    try {
+        if (AssignmentGroup.course_id == CourseInfo.id) throw `Match`;
+        else {
+            catch(err) {
+                console.log(err)
+            }
+        }
+
+    }
+}
+
+//if (AssignmentGroup.course_id !== CourseInfo.id){
+//      console.log(`Error-Invalid Input`)
+//}
+
+// if (AssignmentGroup.assignments[i].due_at <)
+
+if (LearnerSubmission[i].submission.submitted_at > AssignmentGroup.assignments[i].due_at)
+       function lateScore(score, dec_percent_off = 0.1, points_possible){
+            let newScore = score - (dec_percent_off * points_possible)
+       }
+
+//let j = `` 
+
+// if (LearnerSubmissions[i].learner_id !== LearnerSubmissions[i].learner_id); continue;
+//        else {
+//}       for (let j = 0; j > 0; j++){
+//            if (AssignmentGroup.assignments[i].due_at < BLANK)
+    //}       if (LearnerSubmission[i].submission.submitted_at > AssignmentGroup.assignments[i].due_at){
+    //        function pastDueScore(score){
+    //             let lateScore = score * 0.9
+//        
+// 
+
+
+
+
+
+// will have an object called keys called  "avg": number
+                                    //      <assignment_id>: number,
+
+                                    try {
+                                        if (AssignmentGroup.course_id == CourseInfo.id) throw `Match`;
+                                          catch(err)
+                                          console.log(err)
+                                            }
+
+
+
+                                            let sum = 0;
+//   for (let i = 0; i < LearnerSubmissions.length; i++){
+//     if (LearnerSubmissions[i].learner_id !== LearnerSubmissions[i].learner_id) continue; // trying to say skip if it's not the same student but not working
+//         else { 
+            
+//          sum += LearnerSubmissions[i].submission.score
+//         } console.log(sum)
+//         }
+
+if (LearnerSubmissions[i].assignment_id == AssignmentGroup.assignments[i].id){
+    console.log(`Match`)
+
+
+
+
+    if (LearnerSubmissions[i].learner_id == results[0].id){
+        console.log(`Yes`)
+       }    if (){
+               console.log(`Oh`)
+      // }    if (AssignmentGroup.assignments[i].due_at > ){
+       
+       } 
+
+
+
+
+//        //let i = 0;
+// for (let i = 0; i < LearnerSubmissions.length; i++){
+//     Number.LearnerSubmissions[i].submission.submitted_at = []                //this gets a reference error
+//     console.log(Number.submitted_at)
+//     }
+
+
+for (let i = 0; i < LearnerSubmissions.length; i++){         //created a for loop
+    if (LearnerSubmissions[i].assignment_id > 0 ){             //&& LearnerSubmissions[i].learner_id      //---trying to do a conditional?
+        let sumAssignmentIDs = ``
+        sumAssignmentIDs += LearnerSubmissions[i].assignment_id
+        console.log(sumAssignmentIDs)                                        // now I have a string of assignment id's and the first three are for student id 125 and the last two for student id 
+    } 
+        //if (sumAssignmentIDs[1] = AssignmentGroup.assignments[1].id)           //can;t iterate in the same loop because diff length arrrays
+        //    console.log(`Match`)
+ }
--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const CourseInfo = {
   const AssignmentGroup = {
     id: 12345,
     name: "Fundamentals of JavaScript",
-    course_id: 451,
-    group_weight: 25,
+    course_id: 451,     //the ID of the course the assignment group belongs to
+    group_weight: 25,   //the percentage weight of the entire assignment group
     assignments: [
       {
         id: 1,
@@ -19,8 +19,8 @@ const CourseInfo = {
       {
         id: 2,
         name: "Write a Function",
-        due_at: "2023-02-27",
-        points_possible: 150
+        due_at: "2023-02-27",       //the due date
+        points_possible: 150        //max amount of points possible
       },
       {
         id: 3,
@@ -74,28 +74,113 @@ const CourseInfo = {
       }
     }
   ];
+
+
+  try {
+    if (AssignmentGroup.course_id !== CourseInfo.id) throw `Invalid Course ID`;
+   }
+      catch(err){
+      console.log(err)
+  }  
+
+
+
+//   let i = 0;
+//   for (let i = 0; i < LearnerSubmissions.length; i++){
+//   if (LearnerSubmissions[i].assignment_id == AssignmentGroup.assignments[i].id){
+//         if (AssignmentGroup.assignments[i].due_at > x){ //evaluate if due date passed 
+//             let score = 0; let                                   
+//         } else if (LearnerSubmissions[i].submission.submitted_at > AssignmentGroup.assignments[i].due_at) // see if sub date >= sumbit date
+//             function lateScore(score, dec_percent_off = 0.1, points_possible){    //       (function to calculate lower grade)
+//                 let newScore = score - (dec_percent_off * points_possible)
+//        }                                                                       
+//         // else just score
+//         // sum += / points_possible
+//   }
+// }
+
+
+
+// final result stored in 2 objects with keys id:, avg:, 1:, 2:
+
+
+const results = []
+        if (results.includes({id: 125})){           //109 - 117 are making objects for each student
+            console.log(`Id exists`);
+           } else {
+            results.push({id: 125})
+            } 
+         if (results.includes({id: 132})){
+            console.log(`Id exists`);
+         }  else {
+            results.push({id: 132})
+         }  console.log(results)
+
+         for (let i = 0; i < LearnerSubmissions.length; i++){         //created a for loop
+            if (LearnerSubmissions[i].assignment_id > 0 ){             //&& LearnerSubmissions[i].learner_id      //---trying to do a conditional?
+                let sumAssignmentIDs = ``
+                sumAssignmentIDs += LearnerSubmissions[i].assignment_id
+                console.log(sumAssignmentIDs)                                        // now I have a string of assignment id's and the first three are for student id 125 and the last two for student id 
+            } 
+                //if (sumAssignmentIDs[1] = AssignmentGroup.assignments[1].id)           //can;t iterate in the same loop because diff length arrrays
+                //    console.log(`Match`)
+         }
+
+
+         
+        
+       
+
+// function getLearnerData(course, ag, submissions){
+//     const results = []
+//     let avg = 0;
+//     for (let i = 0; i < LearnerSubmissions.length; i++){
+//         if (results.includes({id: 125})){
+//             console.log(`Id exists`);
+//          } else {
+//             results.pop({id: 125})
+//             } console.log(results)
+//         } //is not in results, then create an object for 
+// }       
+
   
-  function getLearnerData(course, ag, submissions) {
-    // here, we would process this data to achieve the desired result.
-    const result = [
-      {
-        id: 125,
-        avg: 0.985, // (47 + 150) / (50 + 150)
-        1: 0.94, // 47 / 50
-        2: 1.0 // 150 / 150
-      },
-      {
-        id: 132,
-        avg: 0.82, // (39 + 125) / (50 + 150)
-        1: 0.78, // 39 / 50
-        2: 0.833 // late: (140 - 15) / 150
-      }
-    ];
+//   function getLearnerData(course, ag, submissions) {
+//     // here, we would process this data to achieve the desired result.
+//     const result = [
+//       {
+//         id: 125,
+//         avg: 0.985, // (47 + 150) / (50 + 150)
+//         1: 0.94, // 47 / 50
+//         2: 1.0 // 150 / 150
+//       },
+//       {
+//         id: 132,
+//         avg: 0.82, // (39 + 125) / (50 + 150)
+//         1: 0.78, // 39 / 50
+//         2: 0.833 // late: (140 - 15) / 150
+//       }
+//     ];
   
-    return result;
-  }
+//     return result;
+//   }
   
-  const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+  //const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+
   
-  console.log(result);
+  //console.log(result);
   
+
+
+
+
+//1.) If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
+// 2.) You should also account for potential errors in the data that your program receives. What if points_possible is 0? You cannot divide by zero. What if a value that you are expecting to be a number is instead a string? 
+// 3.) Use try/catch and other logic to handle these types of errors gracefully.
+// 4.) If an assignment is not yet due, do not include it in the results or the average. Additionally, if the learner’s submission is late (submitted_at is past due_at), deduct 10 percent of the total points possible from their score for that assignment.
+// 5.) Create a function named getLearnerData() that accepts these values as parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission]), and returns the formatted result, which should be an array of objects as described above.
+// 6.) You may use as many helper functions as you see fit.
+
+//if (AssignmentGroup.course_id !== CourseInfo.id){
+//      console.log(`Error-Invalid Input`)
+//}
+
